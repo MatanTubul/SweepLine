83f064d92bc18ad916fd243bea9406c198b4af86
sgibraude <matant86@gmail.com>
2015-04-01 14:42:48 +0300

    Update Main.java

--- SweepLine/src/Main.java
+++ SweepLine/src/Main.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-//check
+//check 2
 public class Main {
 
 public static void main(String[] args) throws IOException {
